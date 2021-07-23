00f8a81a922e2e07e0b4dc128794b900a98506b5
erfan-amani <erfan80amani23@gmail.com>
2021-07-23 22:10:44 +0430

    added cart reducer and addItem logic in reducer for adding new item into cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,42 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+    const updatedItems = [...state.item, action.item];
+    const updatedTotalAmount =
+      state.totalAmount + action.item.amount * action.item.price;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === 'REMOVE_ITEM') {
+  }
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  const addItem = (item) => {};
-  const removeItem = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItem = (item) => {
+    dispatchCartAction({ type: 'ADD_ITEM', item: item });
+  };
+  const removeItem = (id) => {
+    dispatchCartAction({ type: 'REMOVE_ITEM', id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItem,
     removeItem: removeItem,
   };
