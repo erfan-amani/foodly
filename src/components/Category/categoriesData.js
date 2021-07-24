b20112e148327e7e9cabae65ebf0562621362f1f
erfan-amani <erfan80amani23@gmail.com>
2021-07-24 15:11:25 +0430

    extracted categories data in separated file

---
+++ src/components/Category/categoriesData.js
@@ -0,0 +1,38 @@
+export const CATEGORIES_DATA = [
+  {
+    title: 'Pizza',
+    imageSrc: 'pizza.png',
+  },
+  {
+    title: 'Fast food',
+    imageSrc: 'fast-food.png',
+  },
+  {
+    title: 'Indian',
+    imageSrc: 'indian.png',
+  },
+  {
+    title: 'Italian',
+    imageSrc: 'italian.png',
+  },
+  {
+    title: 'Mexican',
+    imageSrc: 'mexican.png',
+  },
+  {
+    title: 'Thai',
+    imageSrc: 'thai.png',
+  },
+  {
+    title: 'Sushi',
+    imageSrc: 'sushi.png',
+  },
+  {
+    title: 'Salad',
+    imageSrc: 'salad.png',
+  },
+  {
+    title: 'Drink',
+    imageSrc: 'drink.png',
+  },
+];
